cacab49052545285fba37bc7e0d56068c176ad16
Amoong <moo1323@naver.com>
2024-03-19 19:59:07 +0900

    Add more difficulty

--- Assets/Enemy/EnemyHealth.cs
+++ Assets/Enemy/EnemyHealth.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Enemy))]
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] int maxHitPoints = 5;
 
-    [SerializeField] int currentHitPoints = 0;
+    [Tooltip("Adds amount to maxHitPoints when enemy dies.")]
+    [SerializeField] int difficultyRamp = 1;
+
+    int currentHitPoints = 0;
 
     Enemy enemy;
 
@@ -30,6 +34,7 @@ public class EnemyHealth : MonoBehaviour
         if (currentHitPoints < 1)
         {
             gameObject.SetActive(false);
+            maxHitPoints += difficultyRamp;
             enemy.RewardGold();
         }
     }
