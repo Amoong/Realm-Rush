0ae7501bff483633fe1e0e8a3b901961c14096e8
Amoong <moo1323@naver.com>
2024-03-19 19:50:55 +0900

    Add Gold UI

--- Assets/Bank/Bank.cs
+++ Assets/Bank/Bank.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,25 +9,36 @@ public class Bank : MonoBehaviour
     [SerializeField] int currentBalance;
     public int CurrentBalance { get { return currentBalance; } }
 
+
+    [SerializeField] TextMeshProUGUI displayBalance;
+
     void Awake()
     {
         currentBalance = startingBalace;
+        UpdateDisplay();
     }
 
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
 
         if (currentBalance < 0)
         {
             // Lose the game;
             ReloadScene();
         }
+    }
+
+    void UpdateDisplay()
+    {
+        displayBalance.text = "Gold: " + currentBalance;
     }
 
     void ReloadScene()
